b175409dd588dc40205e470f578f748bff9c2e18
Rabiya <rabiya@gmail.com>
2025-11-18 09:35:23 -0800

    ....

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,28 +5,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    @vite('resources/css/app.css')
 
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        .navbar {
+            display: flex;
+            font-size: 20px;
+            font-weight: bold;
+            text-decoration: underline;
+            padding: 20px;
+            background-color: #000000;
+            color: white;
+        }
+
+        .nav {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 100%;
+        }
+
+        .nav-items ul {
+            display: flex;
+            gap: 30px;
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        .nav-items a {
+            color: white;
+            text-decoration: none;
+            font-size: 20px;
+            padding: 4px 8px;
+            transition: 0.3s;
+        }
+
+        .nav-items a:hover {
+            color: gray;
+        }
+
+        .logo {
+            letter-spacing: 2px;
+        }
+    </style>
 </head>
 
 <body>
 
-    <div class="navbar flex text-3xl font-bold underline p-10 bg-blue-900">
-        <div class="nav flex justify-between items-center w-full">
+    <div class="navbar">
+        <div class="nav">
             <div class="logo">Logo</div>
+
             <div class="nav-items">
-                <ul class="flex gap-6">
-                    <li><a href="./" class="hover:text-blue-400">Home</a></li>
-                    <li><a href="about.blade.php" class="hover:text-blue-400">About</a></li>
+                <ul>
+                    <li><a href="./">Home</a></li>
+                    <li><a href="about.blade.php">About</a></li>
                 </ul>
             </div>
-            <div class="toggle">
-                toggle
-            </div>
+
+            <div class="toggle">toggle</div>
         </div>
     </div>
 
 </body>
-
 
 </html>
